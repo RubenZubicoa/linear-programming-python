fa1f3d53fc8d27c0a613128361fb3eae104e30f8
RubenZubicoa <rubenzubicoatic@gmail.com>
2023-02-01 20:34:33 +0100

    feat: posible result

--- src/index.py
+++ src/index.py
@@ -74,54 +74,54 @@ for variable in prob.variables():
         nb[name] =  nb[name] * variable.varValue
         sn[name] =  sn[name] * variable.varValue
 
-material_vars2 = LpVariable.dicts("Component", components, lowBound=0, cat="Continuos")
+material_vars2 = LpVariable.dicts("Component", materials, lowBound=0, cat="Continuos")
 
 prob2 += lpSum([costs[i] * material_vars2[i] for i in materials])
 
 prob2 += lpSum([carbono[i] * material_vars2[i] for i in materials]) >= 2.5, "carbonoMin"
 prob2 += lpSum([carbono[i] * material_vars2[i] for i in materials]) <= 4.2, "carbonoMax"
 
-prob2 += lpSum([si[i] * material_vars2[i] for i in materials]) >= 0.2, "siMin"
-prob2 += lpSum([si[i] * material_vars2[i] for i in materials]) <= 1.9, "siMax"
+# prob2 += lpSum([si[i] * material_vars2[i] for i in materials]) >= 0.2, "siMin"
+# prob2 += lpSum([si[i] * material_vars2[i] for i in materials]) <= 1.9, "siMax"
 
-prob2 += lpSum([mn[i] * material_vars2[i] for i in materials]) >= 0.15, "mnMin"
-prob2 += lpSum([mn[i] * material_vars2[i] for i in materials]) <= 0.2, "mnMax"
+# prob2 += lpSum([mn[i] * material_vars2[i] for i in materials]) >= 0.15, "mnMin"
+# prob2 += lpSum([mn[i] * material_vars2[i] for i in materials]) <= 0.2, "mnMax"
 
-prob2 += lpSum([s[i] * material_vars2[i] for i in materials]) >= 0, "sMin"
-prob2 += lpSum([s[i] * material_vars2[i] for i in materials]) <= 0.03, "sMax"
+# prob2 += lpSum([s[i] * material_vars2[i] for i in materials]) >= 0, "sMin"
+# prob2 += lpSum([s[i] * material_vars2[i] for i in materials]) <= 0.03, "sMax"
 
-prob2 += lpSum([p[i] * material_vars2[i] for i in materials]) >= 0, "pMin"
-prob2 += lpSum([p[i] * material_vars2[i] for i in materials]) <= 0.03, "pMax"
+# prob2 += lpSum([p[i] * material_vars2[i] for i in materials]) >= 0, "pMin"
+# prob2 += lpSum([p[i] * material_vars2[i] for i in materials]) <= 0.03, "pMax"
 
-prob2 += lpSum([mg[i] * material_vars2[i] for i in materials]) >= 0, "mgMin"
-prob2 += lpSum([mg[i] * material_vars2[i] for i in materials]) <= 0.06, "mgMax"
+# prob2 += lpSum([mg[i] * material_vars2[i] for i in materials]) >= 0, "mgMin"
+# prob2 += lpSum([mg[i] * material_vars2[i] for i in materials]) <= 0.06, "mgMax"
 
-prob2 += lpSum([cu[i] * material_vars2[i] for i in materials]) >= 0, "cuMin"
-prob2 += lpSum([cu[i] * material_vars2[i] for i in materials]) <= 0.01, "cuMax"
+# prob2 += lpSum([cu[i] * material_vars2[i] for i in materials]) >= 0, "cuMin"
+# prob2 += lpSum([cu[i] * material_vars2[i] for i in materials]) <= 0.01, "cuMax"
 
-prob2 += lpSum([cr[i] * material_vars2[i] for i in materials]) >= 0, "crMin"
-prob2 += lpSum([cr[i] * material_vars2[i] for i in materials]) <= 0.01, "crMax"
+# prob2 += lpSum([cr[i] * material_vars2[i] for i in materials]) >= 0, "crMin"
+# prob2 += lpSum([cr[i] * material_vars2[i] for i in materials]) <= 0.01, "crMax"
 
-prob2 += lpSum([ni[i] * material_vars2[i] for i in materials]) >= 0, "niMin"
-prob2 += lpSum([ni[i] * material_vars2[i] for i in materials]) <= 0.015, "niMax"
+# prob2 += lpSum([ni[i] * material_vars2[i] for i in materials]) >= 0, "niMin"
+# prob2 += lpSum([ni[i] * material_vars2[i] for i in materials]) <= 0.015, "niMax"
 
-prob2 += lpSum([ti[i] * material_vars2[i] for i in materials]) >= 0, "tiMin"
-prob2 += lpSum([ti[i] * material_vars2[i] for i in materials]) <= 0.005, "tiMax"
+# prob2 += lpSum([ti[i] * material_vars2[i] for i in materials]) >= 0, "tiMin"
+# prob2 += lpSum([ti[i] * material_vars2[i] for i in materials]) <= 0.005, "tiMax"
 
-prob2 += lpSum([v[i] * material_vars2[i] for i in materials]) >= 0, "vMin"
-prob2 += lpSum([v[i] * material_vars2[i] for i in materials]) <= 0.005, "vMax"
+# prob2 += lpSum([v[i] * material_vars2[i] for i in materials]) >= 0, "vMin"
+# prob2 += lpSum([v[i] * material_vars2[i] for i in materials]) <= 0.005, "vMax"
 
-prob2 += lpSum([mo[i] * material_vars2[i] for i in materials]) >= 0, "moMin"
-prob2 += lpSum([mo[i] * material_vars2[i] for i in materials]) <= 0.005, "moMax"
+# prob2 += lpSum([mo[i] * material_vars2[i] for i in materials]) >= 0, "moMin"
+# prob2 += lpSum([mo[i] * material_vars2[i] for i in materials]) <= 0.005, "moMax"
 
-prob2 += lpSum([w[i] * material_vars2[i] for i in materials]) >= 0, "wMin"
-prob2 += lpSum([w[i] * material_vars2[i] for i in materials]) <= 0.002, "wMax"
+# prob2 += lpSum([w[i] * material_vars2[i] for i in materials]) >= 0, "wMin"
+# prob2 += lpSum([w[i] * material_vars2[i] for i in materials]) <= 0.002, "wMax"
 
-prob2 += lpSum([nb[i] * material_vars2[i] for i in materials]) >= 0, "nbMin"
-prob2 += lpSum([nb[i] * material_vars2[i] for i in materials]) <= 0.002, "nbMax"
+# prob2 += lpSum([nb[i] * material_vars2[i] for i in materials]) >= 0, "nbMin"
+# prob2 += lpSum([nb[i] * material_vars2[i] for i in materials]) <= 0.002, "nbMax"
 
-prob2 += lpSum([sn[i] * material_vars2[i] for i in materials]) >= 0, "snMin"
-prob2 += lpSum([sn[i] * material_vars2[i] for i in materials]) <= 0.002, "snMax"
+# prob2 += lpSum([sn[i] * material_vars2[i] for i in materials]) >= 0, "snMin"
+# prob2 += lpSum([sn[i] * material_vars2[i] for i in materials]) <= 0.002, "snMax"
 
 prob2.solve()
 
--- src/index2.py
+++ src/index2.py
@@ -44,15 +44,56 @@ for material in materials:
 
 prob += lpSum([1 * material_vars[i] for i in materials]) == 100
 
+# for i in materials:
+#     prob += lpSum((carbono[i] * material_vars[i]) / 100) >= 2.5
+#     prob += lpSum((carbono[i] * material_vars[i]) / 100) <= 4.2
+
+#     prob += lpSum((si[i] * material_vars[i]) / 100) >= 0.2
+#     prob += lpSum((si[i] * material_vars[i]) / 100) <= 1.9
+
+#     prob += lpSum((mn[i] * material_vars[i]) / 100) >= 0.15
+#     prob += lpSum((mn[i] * material_vars[i]) / 100) <= 0.2
+
+prob += lpSum([carbono[i] * material_vars[i] for i in materials]) >= 2.5 * 100, "carbonoMin"
+prob += lpSum([carbono[i] * material_vars[i] for i in materials]) <= 4.2 * 100, "carbonoMax"
+prob += lpSum([si[i] * material_vars[i] for i in materials]) >= 0.2 * 100, "siMin"
+prob += lpSum([si[i] * material_vars[i] for i in materials]) <= 1.9 * 100, "siMax"
+prob += lpSum([mn[i] * material_vars[i] for i in materials]) >= 0.15 * 100, "mnMin"
+prob += lpSum([mn[i] * material_vars[i] for i in materials]) <= 0.2 * 100, "mnMax"
+prob += lpSum([s[i] * material_vars[i] for i in materials]) >= 0, "sMin * 100"
+prob += lpSum([s[i] * material_vars[i] for i in materials]) <= 0.03 * 100, "sMax"
+prob += lpSum([p[i] * material_vars[i] for i in materials]) >= 0, "pMin * 100"
+prob += lpSum([p[i] * material_vars[i] for i in materials]) <= 0.03 * 100, "pMax"
+prob += lpSum([mg[i] * material_vars[i] for i in materials]) >= 0, "mgMin * 100"
+prob += lpSum([mg[i] * material_vars[i] for i in materials]) <= 0.06 * 100, "mgMax"
+prob += lpSum([cu[i] * material_vars[i] for i in materials]) >= 0, "cuMin * 100"
+prob += lpSum([cu[i] * material_vars[i] for i in materials]) <= 0.01 * 100, "cuMax"
+prob += lpSum([cr[i] * material_vars[i] for i in materials]) >= 0, "crMin * 100"
+prob += lpSum([cr[i] * material_vars[i] for i in materials]) <= 0.01 * 100, "crMax"
+prob += lpSum([ni[i] * material_vars[i] for i in materials]) >= 0, "niMin * 100"
+prob += lpSum([ni[i] * material_vars[i] for i in materials]) <= 0.015 * 100, "niMax"
+prob += lpSum([ti[i] * material_vars[i] for i in materials]) >= 0, "tiMin * 100"
+prob += lpSum([ti[i] * material_vars[i] for i in materials]) <= 0.005 * 100, "tiMax"
+prob += lpSum([v[i] * material_vars[i] for i in materials]) >= 0, "vMin * 100"
+prob += lpSum([v[i] * material_vars[i] for i in materials]) <= 0.005 * 100, "vMax"
+prob += lpSum([mo[i] * material_vars[i] for i in materials]) >= 0, "moMin * 100"
+prob += lpSum([mo[i] * material_vars[i] for i in materials]) <= 0.005 * 100, "moMax"
+prob += lpSum([w[i] * material_vars[i] for i in materials]) >= 0, "wMin * 100"
+prob += lpSum([w[i] * material_vars[i] for i in materials]) <= 0.002 * 100, "wMax"
+prob += lpSum([nb[i] * material_vars[i] for i in materials]) >= 0, "nbMin * 100"
+prob += lpSum([nb[i] * material_vars[i] for i in materials]) <= 0.002 * 100, "nbMax"
+prob += lpSum([sn[i] * material_vars[i] for i in materials]) >= 0, "snMin * 100"
+prob += lpSum([sn[i] * material_vars[i] for i in materials]) <= 0.002 * 100, "snMax"
+
 prob.solve()
 
 prueba = {}
 
 for v in prob.variables():
-    #if v.varValue>0:
+    if v.varValue>0:
         print(v.name, "=", v.varValue)
-        print(str(v.name)[9:])
+        #print(str(v.name)[9:])
         prueba[v.name] = v.varValue
 
-obj = value(prob.objective)
-print("The total cost of this balanced diet is: ${}".format(round(obj,2)))
+obj = value(prob.objective) / 100
+print("The total cost is: ${}".format(round(obj,2)))
