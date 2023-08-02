9e35f8e9cc739d38ed6e91c7d932b74d36c5839e
Ruben <rubenzubicoatic@gmail.com>
2023-08-02 10:11:42 +0200

    refactor: improve the code

--- app.py
+++ app.py
@@ -2,9 +2,6 @@ from pulp import *
 from flask import  Flask, jsonify, request
 app = Flask(__name__)
 
-
-
-
 import pyodbc
 
 server = 'castingcalc.database.windows.net' # to specify an alternate port
@@ -98,41 +95,39 @@ def solve(idFormula, casting):
     prob += lpSum([si[i] * material_vars[i] for i in materials]) <= zzMax.Si * 100, "siMax"
     prob += lpSum([mn[i] * material_vars[i] for i in materials]) >= zzMin.Mn * 100, "mnMin"
     prob += lpSum([mn[i] * material_vars[i] for i in materials]) <= zzMax.Mn * 100, "mnMax"
-    prob += lpSum([s[i] * material_vars[i] for i in materials]) >= zzMin.S * 100, "sMin * 100"
+    prob += lpSum([s[i] * material_vars[i] for i in materials]) >= zzMin.S * 100, "sMin"
     prob += lpSum([s[i] * material_vars[i] for i in materials]) <= zzMax.S * 100, "sMax"
-    prob += lpSum([p[i] * material_vars[i] for i in materials]) >= zzMin.P * 100, "pMin * 100"
+    prob += lpSum([p[i] * material_vars[i] for i in materials]) >= zzMin.P * 100, "pMin"
     prob += lpSum([p[i] * material_vars[i] for i in materials]) <= zzMax.P * 100, "pMax"
-    prob += lpSum([mg[i] * material_vars[i] for i in materials]) >= zzMin.Mg * 100, "mgMin * 100"
+    prob += lpSum([mg[i] * material_vars[i] for i in materials]) >= zzMin.Mg * 100, "mgMin"
     prob += lpSum([mg[i] * material_vars[i] for i in materials]) <= zzMax.Mg * 100, "mgMax"
-    prob += lpSum([cu[i] * material_vars[i] for i in materials]) >= zzMin.Cu * 100, "cuMin * 100"
+    prob += lpSum([cu[i] * material_vars[i] for i in materials]) >= zzMin.Cu * 100, "cuMin"
     prob += lpSum([cu[i] * material_vars[i] for i in materials]) <= zzMax.Cu * 100, "cuMax"
-    prob += lpSum([cr[i] * material_vars[i] for i in materials]) >= zzMin.Cr * 100, "crMin * 100"
+    prob += lpSum([cr[i] * material_vars[i] for i in materials]) >= zzMin.Cr * 100, "crMin"
     prob += lpSum([cr[i] * material_vars[i] for i in materials]) <= zzMax.Cr * 100, "crMax"
-    prob += lpSum([ni[i] * material_vars[i] for i in materials]) >= zzMin.Ni * 100, "niMin * 100"
+    prob += lpSum([ni[i] * material_vars[i] for i in materials]) >= zzMin.Ni * 100, "niMin"
     prob += lpSum([ni[i] * material_vars[i] for i in materials]) <= zzMax.Ni * 100, "niMax"
-    prob += lpSum([ti[i] * material_vars[i] for i in materials]) >= zzMin.Ti * 100, "tiMin * 100"
+    prob += lpSum([ti[i] * material_vars[i] for i in materials]) >= zzMin.Ti * 100, "tiMin"
     prob += lpSum([ti[i] * material_vars[i] for i in materials]) <= zzMax.Ti * 100, "tiMax"
-    prob += lpSum([v[i] * material_vars[i] for i in materials]) >= zzMin.V * 100, "vMin * 100"
+    prob += lpSum([v[i] * material_vars[i] for i in materials]) >= zzMin.V * 100, "vMin"
     prob += lpSum([v[i] * material_vars[i] for i in materials]) <= zzMax.V * 100, "vMax"
-    prob += lpSum([mo[i] * material_vars[i] for i in materials]) >= zzMin.Mo * 100, "moMin * 100"
+    prob += lpSum([mo[i] * material_vars[i] for i in materials]) >= zzMin.Mo * 100, "moMin"
     prob += lpSum([mo[i] * material_vars[i] for i in materials]) <= zzMax.Mo * 100, "moMax"
-    prob += lpSum([w[i] * material_vars[i] for i in materials]) >= zzMin.W * 100, "wMin * 100"
+    prob += lpSum([w[i] * material_vars[i] for i in materials]) >= zzMin.W * 100, "wMin"
     prob += lpSum([w[i] * material_vars[i] for i in materials]) <= zzMax.W * 100, "wMax"
-    prob += lpSum([nb[i] * material_vars[i] for i in materials]) >= zzMin.Nb * 100, "nbMin * 100"
+    prob += lpSum([nb[i] * material_vars[i] for i in materials]) >= zzMin.Nb * 100, "nbMin"
     prob += lpSum([nb[i] * material_vars[i] for i in materials]) <= zzMax.Nb * 100, "nbMax"
-    prob += lpSum([sn[i] * material_vars[i] for i in materials]) >= zzMin.Sn * 100, "snMin * 100"
+    prob += lpSum([sn[i] * material_vars[i] for i in materials]) >= zzMin.Sn * 100, "snMin"
     prob += lpSum([sn[i] * material_vars[i] for i in materials]) <= zzMax.Sn * 100, "snMax"
 
     prob.solve()
 
-    prueba = {}
 
     for v in prob.variables():
         name = str(v.name).split("_")[1]
         if v.varValue>0:
             print(name, "=", v.varValue)
             #print(str(v.name)[9:])
-            prueba[name] = v.varValue
             cursor.execute("update formulas_simulacion set optimum = "+str(v.varValue)+" where IDmaterial = '"+name+"' and Idformula = '" + idFormula +"' and Casting = '"+ casting +"';")
         else:
             cursor.execute("update formulas_simulacion set optimum = 0 where IDmaterial = '"+name+"' and Idformula = '" + idFormula +"' and Casting = '"+ casting +"';")
